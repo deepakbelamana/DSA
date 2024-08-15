911518982caceb79cc7dbfd6401e8331914fce08
deepak belamana <saideepak627@gmail.com>
2024-08-15 22:13:43 +0530

    week 2 15/08

---
+++ src/com/miscellaneous/SelectionSort.java
@@ -0,0 +1,17 @@
+package com.miscellaneous;
+
+public class SelectionSort {
+    void selectionSort(int a[], int n)
+    {
+        for(int i=0;i<n-1;i++){
+            int min=a[i];
+            for(int j=i+1;j<n;j++){
+                if(a[j]<a[i]){
+                    int temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+    }
+}
