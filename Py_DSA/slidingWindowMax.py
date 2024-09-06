62051d9c470f1a99a673a790abd840ba413d9cca
deepak belamana <saideepak627@gmail.com>
2024-09-07 01:20:35 +0530

    week 3 07/09

---
+++ Py_DSA/slidingWindowMax.py
@@ -0,0 +1,24 @@
+import collections
+from collections import deque
+
+
+def max_of_subarrays(nums, n, k):
+    output = []
+    q = deque()  # index
+    l = r = 0
+    while r < n:
+        while q and nums[q[-1]] < nums[r]:
+            q.pop()
+        q.append(r)
+        # remove Left val from window
+        if l > q[0]:
+            q.popleft()
+        if (r + 1) >= k:
+            output.append(nums[q[0]])
+            l += 1
+        r += 1
+    return output
+
+
+if __name__ == '__main__':
+    print(max_of_subarrays([8,7,6,9], 4, 2))
