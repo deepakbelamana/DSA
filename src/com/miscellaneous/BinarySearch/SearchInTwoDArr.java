4355622778abb98f2886a184d780d2c625ef67d5
deepak belamana <saideepak627@gmail.com>
2024-09-17 23:57:10 +0530

    week 4 17/09

--- src/com/miscellaneous/BinarySearch/SearchInTwoDArr.java
+++ src/com/miscellaneous/BinarySearch/SearchInTwoDArr.java
@@ -1,30 +1,36 @@
 package com.miscellaneous.BinarySearch;
 
 public class SearchInTwoDArr {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int arrSize = 0;
-        for (int j = 0; j < matrix[0].length; j++) {
-            arrSize++;
-        }
-        int[] oneD = new int[arrSize * matrix.length];
-        int a = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                oneD[a] = matrix[i][j];
-                a++;
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int[] start = new int[]{0,0};
+        int[] end = new int[]{m-1,n-1};
+        while(start[0]<=end[0]){
+            int distance=end[0]*(n)+(end[1]+1)-(start[1]);
+            int midDistance=distance/2;
+            int[] mid = new int[2];
+            int i=start[0];
+            int j=start[1];
+            for(int k=0;k<midDistance;k++){
+                j++;
+                if(j>n-1){
+                    j=0;
+                    i++;
+                }
             }
-        }
-        int start = 0;
-        int end = oneD.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (oneD[mid] == target) {
+            mid[0]=i;
+            mid[1]=j;
+            if(matrix[mid[0]][mid[1]]==target){
                 return true;
             }
-            if (oneD[mid] > target) {
-                end = mid - 1;
-            } else if (oneD[mid] < target) {
-                start = mid + 1;
+            if(matrix[mid[0]][mid[1]]<target){
+                start[0]=mid[0];
+                start[1]=mid[1]+1;
+            }
+            else if(matrix[mid[0]][mid[1]]>target){
+                end[0]=mid[0];
+                end[1]=mid[1]-1;
             }
         }
         return false;
--- src/com/miscellaneous/Main/Main.java
+++ src/com/miscellaneous/Main/Main.java
@@ -1,6 +1,7 @@
 package com.miscellaneous.Main;
 
 import com.miscellaneous.Arrays.*;
+import com.miscellaneous.BinarySearch.SearchInTwoDArr;
 import com.miscellaneous.BinarySearch.SingleElementInSortedArray;
 import com.miscellaneous.sorting.CheckSortedAndRotated;
 import com.miscellaneous.sorting.MergeSort;
@@ -11,7 +12,11 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(SingleElementInSortedArray.singleNonDuplicate(new int[]{3,3,7,7,10,11,11}));
+        System.out.println(SearchInTwoDArr.searchMatrix(new int[][]{
+                {1,3,5,7},
+                {10,11,16,20},
+                {23,30,34,60}
+        },13));
 
     }
 }
